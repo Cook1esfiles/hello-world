2b20b87a5834ac967f472420434c6274835dff27
Cookiesfiles <workilliadutyi@gmail.com>
2025-03-21 12:10:54 +0000

    Add HelloWorldCopy.java

--- HelloWorldCopy.java
+++ HelloWorldCopy.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello from conflict-branch!");
+        System.out.println("Hello from main branch");
     }
 }
+
+
+
 
 
